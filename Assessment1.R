6822c6d4ebcd29b61738024087b33d172fc3a4a0
Xevimerino <ikit4@hotmail.com>
2014-08-13 15:57:17 -0600

    Updating code, more parts of the assignment finished. Last chunk of code needs revision

--- Assessment1.R
+++ Assessment1.R
@@ -1,7 +1,12 @@
+#Setting the local language to english, to work with weekdays
+Sys.setlocale("LC_TIME", "English")
+
 #Unzipping and reading the data
 unzip("activity.zip", exdir="./Data")
 activity<-read.csv("./Data/activity.csv")
 
+#Storing the activity intervals as a variable for later use
+intervalsnum<-as.numeric(unique(activity[,"interval"]))
 #Converting the interval column into time format
 temp2 <- mapply(function(x, y) paste0(rep(x, y), collapse = ""), 0, 4 - nchar(activity[,"interval"]))
 activity[,"interval"]<-paste0(temp2, activity[,"interval"])
@@ -10,12 +15,64 @@ activity[,"interval"]<-format(strptime(activity[,"interval"], format="%H%M"), fo
 #Converting the date column into date format
 activity[,"date"]<-strftime(activity[,"date"],format="%Y-%m-%d")
 
-#calculate nºsteps per day
+#Getting vectors with all the diferent dates and times 
+dates<-as.Date(unique(activity[,"date"]))
+intervals<-unique(activity[,"interval"])
+
+#calculate nºsteps per day, histogram, mean and median
 steps<-mapply(function(x) sum(activity[activity[,"date"]==x,"steps"],na.rm=T), dates)
 hist(steps, xlab="number of steps per day",main="Steps per day")
+mean(steps)
+median(steps)
 
-#Calculate mean for each day
-means<-mapply(function(x) mean(activity[activity[,"date"]==x,"steps"],na.rm=T), dates)
-hist(means, xlab="mean steps per day",main="Mean steps per day")
+#Calculate mean nº of steps at each time point
+stepstime<-mapply(function(x) mean(activity[activity[,"interval"]==x,"steps"],na.rm=T), intervals)
+plot(intervalsnum,stepstime,type="l",axes=FALSE,xlab="Time of the day",ylab="Mean number of steps across all days")
+  axis(side=1,at=c(0,500,1000,1500,2000,2359), labels=c("00:00","05:00","10:00","15:00","20:00","23:59"))
+  axis(side=2,at=c(0,50,100,150,200), c(0,50,100,150,200))
+#At which time of the day does the maximum steps happen
+which.max(stepstime)
 
-?hist
+#How many rows have NA's'
+Nodata<-is.na(activity[,"steps"])
+length(Nodata[Nodata==TRUE])
+
+#A vector is created where all the "NA" values are substituted by the mean of the
+#same time point across all days
+correctedsteps <- ifelse(Nodata, stepstime, activity[,"steps"])
+
+#New dataset changing the steps column by correctedsteps
+correctedactivity<-cbind(correctedsteps,activity[,c(2,3)])
+correctedactivity[,"date"]<-as.Date(correctedactivity[,"date"])
+
+#calculate nºsteps per day, histogram, mean and median
+steps2<-mapply(function(x) sum(correctedactivity[correctedactivity[,"date"]==x,"correctedsteps"],na.rm=T), dates)
+hist(steps2, xlab="number of steps per day",main="Steps per day")
+mean(steps2)
+median(steps2)
+
+#Coonverting the dates to weekdays and weekends
+correctedactivity[,"date"]<-weekdays(correctedactivity[,"date"])
+correctedactivity[,"date"]<- ifelse(correctedactivity[,"date"]=="Saturday", "weekend", correctedactivity[,"date"])
+correctedactivity[,"date"]<- ifelse(correctedactivity[,"date"]=="Sunday", "weekend", correctedactivity[,"date"])
+correctedactivity[,"date"]<- as.factor(ifelse(correctedactivity[,"date"]=="weekend", correctedactivity[,"date"],"weekday"))
+
+CAweekday<-correctedactivity[correctedactivity["date"]=="weekday",]
+CAweekend<-correctedactivity[correctedactivity["date"]=="weekend",]
+
+#CODE TO REVISE
+par(mfrow=c(2,1))
+stepstimeWD<-mapply(function(x) mean(CAweekday[CAweekday[,"interval"]==x,"steps2"],na.rm=T), intervals)
+    plot(intervalsnum,stepstimeWD,type="l",axes=FALSE,
+         xlab="Time of the day",ylab="Mean number of steps across all days")
+    axis(side=1,at=c(0,500,1000,1500,2000,2359), 
+         labels=c("00:00","05:00","10:00","15:00","20:00","23:59"))
+    axis(side=2,at=c(0,50,100,150,200), c(0,50,100,150,200))
+stepstimeWE<-mapply(function(x) mean(CAweekend[CAweekend[,"interval"]==x,"steps2"],na.rm=T), intervals)
+    plot(intervalsnum,stepstimeWE,type="l",axes=FALSE,
+         xlab="Time of the day",ylab="Mean number of steps across all days")
+    axis(side=1,at=c(0,500,1000,1500,2000,2359), 
+         labels=c("00:00","05:00","10:00","15:00","20:00","23:59"))
+    axis(side=2,at=c(0,50,100,150,200), c(0,50,100,150,200))
+
+ 
